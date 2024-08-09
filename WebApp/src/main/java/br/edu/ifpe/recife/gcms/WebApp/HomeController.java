16bb348070e09f520069d31c8c4f18fda19e74ca
juliadidra <juliadidra22@gmail.com>
2024-08-09 17:26:10 -0300

    commit from copy

--- WebApp/src/main/java/br/edu/ifpe/recife/gcms/WebApp/HomeController.java
+++ WebApp/src/main/java/br/edu/ifpe/recife/gcms/WebApp/HomeController.java
@@ -11,6 +11,11 @@ public class HomeController {
 	return "Hello, World";
 	}
 	
+	@RequestMapping("/")
+	public @ResponseBody String greeting() {
+	return "Hello, change from 1 copy";
+	}
+	
 	@RequestMapping("/julia")
 	public @ResponseBody String greetingFulano() {
 	return "Hello, Julia Didra!";
